0ddab52417b3368df46b78271f92c124f18cf170
abinayastella <abinayastella@iattechologies.com>
2025-02-13 07:26:33 +0530

    first commit mg

---
+++ Git.java
@@ -0,0 +1,22 @@
+package com.git;
+
+public class Git {
+	private void studentname() {
+		System.out.println("abi");
+	}
+
+	private void studentid() {
+		System.out.println(12);
+	}
+
+	private void studentnumber() {
+		System.out.println(9876543211l);
+	}
+
+	public static void main(String[] args) {
+		Git s=new Git();
+		s.studentname();
+		s.studentid();
+		s.studentnumber();
+	}
+}
